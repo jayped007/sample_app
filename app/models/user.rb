8fa1d12d6403d5ee9a72c9a3b11243f7ce7d445d
Jay Pedersen <jaypedersen@yahoo.com>
2012-12-24 12:13:03 -0600

    ch6 - basic User model

---
+++ app/models/user.rb
@@ -0,0 +1,29 @@
+# == Schema Information
+#
+# Table name: users
+#
+#  id         :integer         not null, primary key
+#  name       :string(255)
+#  email      :string(255)
+#  created_at :datetime        not null
+#  updated_at :datetime        not null
+#
+
+class User < ActiveRecord::Base
+  attr_accessible :email, :name, :password, :password_confirmation
+  VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
+
+  has_secure_password
+  # ==>
+  #   1. require the presence of the password
+  #   2. require that they match
+  #   3. authenticate method to compare encrypted password to password_digest
+
+  validates :name,  presence: true, length: { maximum: 50 }
+  validates :email, presence: true, format: { with: VALID_EMAIL_REGEX },
+                    uniqueness: { case_sensitive: false }
+  validates :password, presence: true, length: { minimum: 6 }
+  validates :password_confirmation, presence: true
+
+  before_save { |user| user.email = email.downcase }
+end
