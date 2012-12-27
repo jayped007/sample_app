4cfaab72749f8b0c4fc78270660a4c19364c34b0
Jay Pedersen <jaypedersen@yahoo.com>
2012-12-27 00:10:17 -0600

    ch8 -- signin and signout, cookies, session

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -7,11 +7,12 @@ class UsersController < ApplicationController
     @user = User.new
   end
 
-def create
+  def create
     @user = User.new(params[:user])
     if @user.save
-      flash[:success] = "Welcome to my app."
-      redirect_to @user # successful save.
+      sign_in @user
+      flash[:success] = "Welcome to the Sample App!"
+      redirect_to @user
     else
       render 'new'
     end
